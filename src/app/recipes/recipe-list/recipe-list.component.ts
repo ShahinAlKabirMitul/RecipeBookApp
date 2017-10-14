6b4b5b2abcdbe80cd81069618bc40da96453e011
mitulbd <mitul.cse@gmail.com>
2017-10-14 10:03:28 +0600

    Design Recipe & Recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,5 @@
+
+import { Recipe } from '../../models/recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes:Recipe[]=[
+    new Recipe('A Recipe','This is simply test','https://get.pxhere.com/photo/dish-meal-food-salad-spring-red-vegetable-corn-plate-recipe-yellow-healthy-eat-lunch-cuisine-delicious-rice-pan-cook-asian-food-fry-up-wok-dish-wok-cool-image-garnish-nutrition-vegetables-court-cool-photo-oil-vegetarian-food-asparagus-gastronomy-paprika-spring-onion-foodfoto-vegetable-pan-halloumi-diet-food-1410966.jpg'),
+    new Recipe('A Recipe','This is simply test','https://get.pxhere.com/photo/dish-meal-food-salad-spring-red-vegetable-corn-plate-recipe-yellow-healthy-eat-lunch-cuisine-delicious-rice-pan-cook-asian-food-fry-up-wok-dish-wok-cool-image-garnish-nutrition-vegetables-court-cool-photo-oil-vegetarian-food-asparagus-gastronomy-paprika-spring-onion-foodfoto-vegetable-pan-halloumi-diet-food-1410966.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
