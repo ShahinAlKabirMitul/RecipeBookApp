6a933b74eafe6a1a2e7d14b3c9daed68bc4c0577
mitulbd <mitul.cse@gmail.com>
2017-10-15 22:35:25 +0600

    add recipe ingedients

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from './indredient.model';
 export class Recipe{
     public name:string;
     public description:string;
     public imagePath:string;
+    public ingredients:Ingredient[];
 
 
-    constructor(name:string,desc:string,imgPath:string){
+
+    constructor(name:string,desc:string,imgPath:string,ing:Ingredient[]){
         this.name=name;
         this.imagePath=imgPath;
-        this.description=desc;    
+        this.description=desc;  
+        this.ingredients=ing;  
     }
 }
--- src/app/servces/recipe.service.ts
+++ src/app/servces/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../models/indredient.model';
 import { Recipe } from './../models/recipe.model';
 import { EventEmitter, Injectable } from '@angular/core';
 
@@ -5,8 +6,20 @@ import { EventEmitter, Injectable } from '@angular/core';
 export class RecipeService {
  public recipeSelect=new EventEmitter<Recipe>();
  private recipes:Recipe[]=[
-    new Recipe('A Recipe','This is simply test','https://get.pxhere.com/photo/dish-meal-food-salad-spring-red-vegetable-corn-plate-recipe-yellow-healthy-eat-lunch-cuisine-delicious-rice-pan-cook-asian-food-fry-up-wok-dish-wok-cool-image-garnish-nutrition-vegetables-court-cool-photo-oil-vegetarian-food-asparagus-gastronomy-paprika-spring-onion-foodfoto-vegetable-pan-halloumi-diet-food-1410966.jpg'),
-    new Recipe('Another Recipe','This is simply test','https://get.pxhere.com/photo/dish-meal-food-salad-spring-red-vegetable-corn-plate-recipe-yellow-healthy-eat-lunch-cuisine-delicious-rice-pan-cook-asian-food-fry-up-wok-dish-wok-cool-image-garnish-nutrition-vegetables-court-cool-photo-oil-vegetarian-food-asparagus-gastronomy-paprika-spring-onion-foodfoto-vegetable-pan-halloumi-diet-food-1410966.jpg')
+    new Recipe('Fried chicken ',
+    'This is simply test',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxNslMoEQlniCTNiK3jw5EKMvuozLSedJtiT3yW3w7Gmj40gqbmw',
+    [
+      new Ingredient("Chicken",1),
+      new Ingredient("Flour",10)
+    ]),
+    new Recipe('BURGER',
+    'This is simply test',
+    'http://www.seriouseats.com/recipes/assets_c/2013/09/20130909-ramen-hacks-new-burger-03-610px-thumb-625xauto-351448.jpg',
+    [
+      new Ingredient("Egg",1),
+      new Ingredient("Onion",10)
+    ])
   ];
   
 getRecipes(){
